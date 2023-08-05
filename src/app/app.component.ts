f0523f49e9f315cd0a3bf94404b2c48787e80a9e
AleksanderBodurri <alexbodurri1117@gmail.com>
2023-08-05 19:02:04 -0400

    skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,52 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: ` <router-outlet></router-outlet> `,
+  template: `
+    <nav>
+      <li>
+        <span class="course">CSCD01</span> Fall 2023
+      </li>
+
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/overview">Overview</a>
+      </li>
+    </nav>
+    <main>
+      <router-outlet/>
+    </main>
+  `,
   styles: [
     `
-      :host {
-        max-width: 1280px;
-        margin: 0 auto;
-        padding: 2rem;
-        text-align: center;
-      }
+    .course {
+      color: var(--blue);
+      font-weight: var(--bold);
+    }
+
+    main {
+      max-width: 80%;
+      margin: 0 auto;
+    }
+
+    a {
+      transition-property: box-shadow;
+      transition-duration: 150ms;
+      transition-timing-function: cubic-bezier(0, 0, 0.4, 1);
+      cursor: pointer;
+      text-decoration: none;
+      outline: transparent solid 2px;
+      outline-offset: 2px;
+      color: #E2E8F0;
+      padding: 0.5rem;
+      font-size: 0.875rem;
+      font-weight: 500;
+    }
+
+    a:focus {
+      box-shadow: 0 0 0 3px rgba(66, 153, 225, 0.6);
+    }
     `,
   ],
 })
