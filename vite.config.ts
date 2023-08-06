1e19c5371bd3f5a651d2cc37c195609340cc3147
AleksanderBodurri <alexbodurri1117@gmail.com>
2023-08-05 20:10:08 -0400

    more work on team page

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@
 
 import { defineConfig } from 'vite';
 import analog from '@analogjs/platform';
+import angular from '@analogjs/vite-plugin-angular';
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,7 +14,12 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     mainFields: ['module'],
   },
-  plugins: [analog()],
+  plugins: [
+    analog(),
+    angular({
+      inlineStylesExtension: 'scss',
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
